ab44e791bebc6765ae7a9e90926e1fb656cd0884
Adam Golinski <awgol@amazon.com>
2022-02-08 22:22:34 +0100

    launch sweep code

--- benchmarking/training_scripts/distilbert_on_imdb/distilbert_on_imdb.py
+++ benchmarking/training_scripts/distilbert_on_imdb/distilbert_on_imdb.py
@@ -15,13 +15,13 @@ DistilBERT fine-tuned on IMDB sentiment classification task
 """
 import argparse
 import logging
+import os
 import time
 
 from syne_tune import Reporter
 from syne_tune.config_space import loguniform, add_to_argparse
 
-
-METRIC_ACCURACY = 'accuracy'
+METRIC_ACCURACY = 'loss'
 
 RESOURCE_ATTR = 'step'
 
@@ -31,20 +31,13 @@ _config_space = {
 }
 
 
-def download_data(config):
-    train_dataset, eval_dataset = load_dataset(
-        'imdb', split=['train', 'test'], cache_dir=config['dataset_path'])
-    return train_dataset, eval_dataset
-
-
 def prepare_data(config, train_dataset, eval_dataset, seed=42):
     # Subsample data
     train_dataset = train_dataset.shuffle(seed=seed).select(range(config['n_train_data']))
     eval_dataset = eval_dataset.shuffle(seed=seed).select(range(config['n_eval_data']))
 
     # Tokenize data
-    tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
-
+    tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased', cache_dir=os.environ["SM_CHANNEL_HFCACHE"])
     def tokenize(batch):
         return tokenizer(batch['text'], padding='max_length', truncation=True)
 
@@ -57,35 +50,42 @@ def prepare_data(config, train_dataset, eval_dataset, seed=42):
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = logits.argmax(axis=-1)
-    metric = load_metric("accuracy")
+    metric = load_metric(f"{os.environ['SM_CHANNEL_HFCACHE']}/accuracy")
     return metric.compute(predictions=predictions, references=labels)
 
 
 def objective(config):
     trial_id = config.get('trial_id')
-    debug_log = trial_id is not None
 
     # Download and prepare data
-    train_dataset, eval_dataset = download_data(config)
+    # train_dataset, eval_dataset = load_dataset(
+    #     'imdb', split=['train', 'test'], keep_in_memory=config['keep_in_memory'],
+    #     cache_dir=os.environ["SM_CHANNEL_HFCACHE"])
+    # # else:
+    train_dataset = load_from_disk(os.environ["SM_CHANNEL_TRAIN"])
+    eval_dataset = load_from_disk(os.environ["SM_CHANNEL_EVAL"])
+
     train_dataset, eval_dataset = prepare_data(config, train_dataset, eval_dataset)
 
     report = Reporter()
 
-    # Do not want to count the time to download the dataset, which can be
-    # substantial the first time
-    ts_start = time.time()
-
     # Download model from Hugging Face model hub
-    model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=2)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        'distilbert-base-uncased', num_labels=2,
+        cache_dir=os.environ["SM_CHANNEL_HFCACHE"])
 
     # Define training args
     training_args = TrainingArguments(
         output_dir='./',
         num_train_epochs=config['epochs'],
-        per_device_train_batch_size=config['train_batch_size'],
-        per_device_eval_batch_size=config['eval_batch_size'],
-        evaluation_strategy='steps',
-        eval_steps=config['eval_interval'] // config['train_batch_size'],
+        per_device_train_batch_size=config['per_device_train_batch_size'],
+        per_device_eval_batch_size=config['per_device_eval_batch_size'],
+        dataloader_num_workers=config['dataloader_num_workers'],
+        evaluation_strategy='steps' if config['eval_interval'] != 0 else 'epoch',
+        eval_steps=config['eval_interval'] // config['per_device_train_batch_size'] if config['eval_interval'] != 0 else 1,
+        logging_strategy='steps',
+        logging_steps=config['log_interval'] if config['log_interval'] != 0 else
+        round(config['n_train_data'] / config['per_device_train_batch_size'] / float(os.environ['SM_NUM_GPUS'])),
         learning_rate=float(config['learning_rate']),
         weight_decay=float(config['weight_decay']),
         load_best_model_at_end=True,
@@ -106,30 +106,43 @@ def objective(config):
 
     # add a callback so that accuracy is sent to Syne Tune whenever it is computed
     class Callback(TrainerCallback):
-        def __init__(self):
-            self.step = 1
+        # def on_evaluate(self, args, state, control, metrics=None, **kwargs):
+        #     # Feed the validation accuracy back to Tune
+        #     # print('metrics', flush=True)
+        #     # print(metrics, flush=True)
+        #     if 'eval_loss' in metrics.keys():
+        #         report_dict = {RESOURCE_ATTR: state.global_step,
+        #                        'loss': metrics['eval_loss']}
+        #         report(**report_dict)
 
-        def on_evaluate(self, args, state, control, metrics, **kwargs):
-            # Feed the validation accuracy back to Tune
-            report_dct = {RESOURCE_ATTR: self.step,
-                          METRIC_ACCURACY: metrics['eval_accuracy']}
-            report(**report_dct)
-            self.step += 1
+        def on_log(self, args, state, control, logs=None, **kwargs):
+            # print('logs', flush=True)
+            # print(logs, flush=True)
+            # {'loss': 0.6808, 'learning_rate': 0.0, 'epoch': 1.0}
+            # {'eval_loss': 0.6770151853561401, 'eval_accuracy': 0.8125, 'eval_runtime': 1.2068, 'eval_samples_per_second': 26.516, 'epoch': 1.0}
+            # {'train_runtime': 206.9605, 'train_samples_per_second': 1.45, 'epoch': 1.0}
+            if 'loss' in logs.keys():
+                report_dict = {RESOURCE_ATTR: state.global_step,
+                               'loss': logs['loss']}
+                report(**report_dict)
+            logs_text = ','.join(f"{k}={v}" for k, v in logs.items())
+            print(f"trial={trial_id},step={state.global_step},{logs_text}", flush=True)
 
     trainer.add_callback(Callback())
+
+    # Do not want to count the time to download the dataset and the model.
+    ts_start = time.time()
 
     # Train model
     trainer.train()
 
     # Evaluate model
-    eval_result = trainer.evaluate(eval_dataset=eval_dataset)
-    eval_accuracy = eval_result['eval_accuracy']
+    # eval_result = trainer.evaluate(eval_dataset=eval_dataset)
+    # eval_accuracy = eval_result['eval_accuracy']
 
     elapsed_time = time.time() - ts_start
 
-    if debug_log:
-        print("Trial {}: accuracy = {:.3f}, elapsed_time = {:.2f}".format(
-            trial_id, eval_accuracy, elapsed_time), flush=True)
+    print(f"elapsed_time={elapsed_time:.2f}", flush=True)
 
 
 if __name__ == '__main__':
@@ -138,19 +151,23 @@ if __name__ == '__main__':
     # only when the code is really called)
     from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments, AutoTokenizer
     from transformers import TrainerCallback
-    from datasets import load_dataset, load_metric
+    import datasets
+    from datasets import load_dataset, load_metric, load_from_disk
 
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_path', type=str, required=True)
-    parser.add_argument('--epochs', type=int, default=3)
-    parser.add_argument('--train_batch_size', type=int, default=16)
-    parser.add_argument('--eval_batch_size', type=int, default=32)
-    parser.add_argument('--n_train_data', type=int, default=25000)
-    parser.add_argument('--n_eval_data', type=int, default=5000)
-    parser.add_argument('--eval_interval', type=int, default=5000)
+    # parser.add_argument('--dataset_path', type=str, required=True)
+    parser.add_argument('--epochs', type=int, default=1)
+    parser.add_argument('--per_device_train_batch_size', type=int, default=16)
+    parser.add_argument('--per_device_eval_batch_size', type=int, default=32)
+    parser.add_argument('--n_train_data', type=int, default=300*16) #25000) # TODO change me
+    parser.add_argument('--n_eval_data', type=int, default=32)  # TODO change me
+    parser.add_argument('--eval_interval', type=int, default=0)
+    parser.add_argument('--log_interval', type=int, default=0)
+    parser.add_argument('--dataloader_num_workers', type=int, default=0)
+    # parser.add_argument('--keep_in_memory', type=int, default=0)
     parser.add_argument('--trial_id', type=str)
     add_to_argparse(parser, _config_space)
     
